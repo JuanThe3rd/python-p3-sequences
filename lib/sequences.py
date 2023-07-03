6a8b5918382d6b7f902f37c2ab5d4b2927dfd599
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-07-03 14:10:08 -0700

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq = []
+
+    for num in range(length):
+        if (num <= 1):
+            fib_seq.append(num)
+        else:
+            fib_seq.append(fib_seq[num-2] + fib_seq[num-1])
+    
+    print(fib_seq)
